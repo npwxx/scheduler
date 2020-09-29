1ebe98fc059af7d15104c7294b9d7f5fd78b09c4
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-28 19:51:15 -0700

    all of the things

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import "./DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+  console.log("here");
+
+  const handleClick = () => {
+    if (setDay) {
+      setDay(name);
+    }
+  };
+
+  return (
+    <li onClick={handleClick} className={dayClass}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">
+        {spots} spot{spots > 1 && "s"} remaining
+      </h3>
+    </li>
+  );
+}
