a4373808ddb8bca6df35c7ec30aec854d3575715
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-29 20:30:54 -0700

    add file & story for Confirm

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -141,5 +142,12 @@ storiesOf("Appointment", module)
       onDelete={action("onDelete")}
       student="Percy Cat"
       interviewer={interviewer}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      onConfirm={action("onConfirm")}
+      onDelete={action("onDelete")}
+      message="Delete the appointment?"
     />
   ));
