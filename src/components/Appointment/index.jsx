76580e3ce3e534c5350f6f244d89604473a80777
Nicole Woodcock <nicole.xox@gmail.com>
2020-10-01 20:50:15 -0700

    add confirm when deleting

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -6,12 +6,14 @@ import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const {
@@ -50,13 +52,20 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={remove}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
       {mode === CREATE && (
         <Form interviewers={interviewers} onSave={save} onCancel={back} />
       )}
       {mode === SAVING && <Status message="Saving" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onConfirm={remove}
+          onCancel={back}
+        />
+      )}
       {mode === DELETING && <Status message="Deleting" />}
     </article>
   );
